72f5039e3ef1288e9ec4bb6ee98620d791910187
Pavlo <oksanakopalko@gmail.com>
2025-03-29 22:03:16 +0200

    не можу взаємодіяти зі сторінкою

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Loader.scss';
+import { wait } from '../../api';
 
-export const Loader: React.FC = () => (
-  <div className="Loader" data-cy="loader">
-    <div className="Loader__content" />
-  </div>
-);
+export const Loader: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = wait(300).then(() => setLoading(false));
+
+    console.log(timer);
+  }, []);
+
+  console.log(loading);
+
+  return (
+    <div className="Loader" data-cy="loader">
+      {loading && <div className="Loader__content" />}
+    </div>
+  );
+};
