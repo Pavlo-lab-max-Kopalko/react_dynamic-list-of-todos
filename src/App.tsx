19c9389dc5cb1bf6901946dfa85e3e38ff7a412f
Pavlo <oksanakopalko@gmail.com>
2025-03-31 14:03:14 +0300

    ще не можу взаємодіяти зі сторінкою

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,19 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { wait } from './api';
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = wait(300).then(() => setLoading(false));
+
+    console.log(timer);
+  }, []);
+
+  console.log(loading);
+
   return (
     <>
       <div className="section">
@@ -21,7 +32,7 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
+              {loading && <Loader />}
               <TodoList />
             </div>
           </div>
